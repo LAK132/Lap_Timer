fc8a9b1f41cba70005d1bee92ef9ff73fc95be5b
LAK132 <lakridesagain@gmail.com>
2014-04-19 21:12:07 +0930

    Major update to ArduinoSerial.cs

--- PedalPrixLapTimer/ArduinoSerial.cs
+++ PedalPrixLapTimer/ArduinoSerial.cs
@@ -14,17 +14,25 @@ namespace PedalPrixLapTimer
         private static bool isSetUp = false; //Whether or not the connection has been set up yet
         private static SerialPort serialPort = new SerialPort(); //The serial port object to use
 
-        public static bool setup(string port, int baud, string echoCode)
+        public static bool setup(string portIn, int baudIn, string echoIn)
         {
-            string echo = "";
+            echo = echoIn;
             if (!isSetUp)
             {
+                port = portIn;
+                baud = baudIn;
+                echo = echoIn;
+                serialPort.PortName = port;
+                serialPort.BaudRate = baud;
+
                 if (serialPort.IsOpen)
                 {
                     serialPort.Close();
                 }
+                
+                serialPort.WriteLine(echo);
 
-                if (echo == echoCode)
+                if (serialPort.ReadLine() == echo)
                 {
                     isSetUp = true;
                     return true;
@@ -63,6 +71,44 @@ namespace PedalPrixLapTimer
             {
                 return "";
             }
+        }
+
+        //converts the given int to a string via a char
+        public static string intToString(int convert)
+        {
+            if (convert <= 126 && convert >= 32)
+            {
+                char convertCh = (char)convert;
+                return convertCh.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //converts the given string to an int via a char
+        public static int[] stringToInt(string convert)
+        {
+            int stringLength = convert.Length;
+            int[] intArray = { 0 };
+            char[] charArray = { (char)0 };
+
+            for(int i = 0; i <= stringLength; i++)
+            {
+                charArray[i] = Convert.ToChar(convert[i]);
+                if (Convert.ToInt16(charArray[i]) <= 126 && Convert.ToInt16(charArray[i]) >= 32)
+                {
+                    intArray[i] = Convert.ToInt16(charArray[i]);
+                }
+                else
+                {
+                    int[] a = { 0 };
+                    return a;
+                }
+            }
+
+            return intArray;
         }
     }
 }
--- PedalPrixLapTimer/LapTimer.cs
+++ PedalPrixLapTimer/LapTimer.cs
@@ -14,7 +14,7 @@ using System.Diagnostics;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
-     {
+    {
 
         Stopwatch swRace = new Stopwatch();     // Used to time total race time
         Stopwatch swLap = new Stopwatch();      // Used to time each lap
