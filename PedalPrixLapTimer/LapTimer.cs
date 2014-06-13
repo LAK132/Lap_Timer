1cca8d0734f13be424aae3a051910be1f21f47de
LAK132 <lakridesagain@gmail.com>
2014-06-13 20:20:16 +0930

    buttonLap_Click cleaned up

--- PedalPrixLapTimer/LapTimer.cs
+++ PedalPrixLapTimer/LapTimer.cs
@@ -139,16 +139,6 @@ namespace PedalPrixLapTimer
 
         private void buttonLap_Click(object sender, EventArgs e)
         {
-            //TimeSpan ts = swLap.Elapsed;
-            ////Format and display the TimeSpan value.
-            //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            //swLap.Reset();
-            //swLap.Start();
-            //riderLaps++;     // increment the number of laps
-            //totalLaps++;     // increment the total laps of the race
-            //richTextBox1.AppendText(System.Environment.NewLine + totalLaps + " ,\t" + riderLaps + " ,\t" + riderDataBase[LVI] + " ,\t" + elapsedTime);//---------------------------------------------
-            //richTextBox1.SelectionStart = richTextBox1.Text.Length;     // Scroll to end of list
-            //richTextBox1.ScrollToCaret();
             lap();
         }
 
